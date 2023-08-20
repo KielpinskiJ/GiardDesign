91e935115e6e9b436c5138cc50699af6c1a1cdf6
Jedrzej Kielpinski <jedrzej.kielpinski@gmail.com>
2023-08-20 23:37:32 +0200

    Added temporary gallery.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,43 @@
+/* eslint-disable no-unused-vars, no-undef */
+document.addEventListener('DOMContentLoaded', function() {
+  var macy = Macy({
+    container: '#macy-container',
+    trueOrder: false,
+    waitForImages: false,
+    margin: 8,
+    columns: 5,
+    breakAt: {
+      576: 2,
+      1920: 3,
+      2560: 4,
+    },
+  });
+});
+
+const expandButton = document.querySelector('.expand-button');
+const gradientOverlay = document.querySelector('.gradient-overlay');
+
+let expanded = false;
+
+expandButton.addEventListener('click', () => {
+  expanded = !expanded;
+  if (expanded) {
+    expandButton.textContent = 'Zwiń';
+    gradientOverlay.style.opacity = '0';
+    document.querySelectorAll('.hidden').forEach(item => item.style.display = 'block');
+  } else {
+    expandButton.textContent = 'Rozwiń';
+    gradientOverlay.style.opacity = '0.8';
+    document.querySelectorAll('.hidden').forEach(item => item.style.display = 'none');
+  }
+  macy.runOnImageLoad(function() {
+    macy.recalculate(true);
+  }, true);
+});
+
+
+
+
 const burgerButton = document.querySelector('.burger-button');
 const mobileMenu = document.querySelector('.mobile-menu');
 const closeButton = document.querySelector('.close-button');
