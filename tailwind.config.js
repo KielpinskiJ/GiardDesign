46d319485bdcb4af808908d1637b4ae9f2e26220
Jedrzej Kielpinski <jedrzej.kielpinski@gmail.com>
2023-08-21 03:59:10 +0200

    Added Instagram section, footer, and improved responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        beige: {
-          200: '#DCC1AB',
-        },
+        'custom-beige': '#DCC1AB',
         'custom-green': '#1B5B31',
         'custom-grey': '#F5F0EC',
+        'custom-black': '#111',
       },
       height: {
         '10v': '10vh',
@@ -24,6 +23,31 @@ module.exports = {
         '100v': '100vh',
       },
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'hd': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      'fullhd': '1920px',
+      // => @media (min-width: 1920px) { ... }
+
+      '2k': '2560px',
+      // => @media (min-width: 2560px) { ... }
+
+      '4k': '3840px',
+      // => @media (min-width: 3840px) { ... }
+    }
   },
   plugins: [],
 };
